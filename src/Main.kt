7056cafa367cd2bef3138aae45f62bd860dd00f8
davidortegan <doni17@gmail.com>
2024-03-12 10:34:14 +0100

    Hacemos un commit y un push

---
+++ src/Main.kt
@@ -0,0 +1,68 @@
+//EJERCICIO 1
+//fun main() {
+//   println("Dime un número: ")
+//    var num: Int = readln().toInt()
+//
+//    if (num < 0){
+//        println("Es un número negativo")
+//    }
+//    else if (num == 0){
+//        println("Ni positivo ni negativo")
+//    }
+//    else{
+//        println("Es un número positivo")
+//    }
+//}
+
+EJERCICIO 2
+fun main(){
+    println("Dame tres número para que te diga si están ordenados de menor a mayor: ")
+    var num1: Int = readln().toInt()
+    var num2: Int = readln().toInt()
+    var num3: Int = readln().toInt()
+
+    if (num1<num2 && num2<num3){
+        println("Están ordenados de menor a mayor")
+    }
+    else {
+        println("No están ordenados de menor a mayor")
+    }
+}
+
+//EJERCICIO 3
+//fun esPrimo(numero: Int): Boolean {
+//    if (numero < 2) return false
+//    for (i in 2..numero / 2) {
+//        if (numero % i == 0) {
+//            return false
+//        }
+//    }
+//    return true
+//}
+//
+//fun mostrarPrimosHasta(n: Int) {
+//    for (numero in 1..n) {
+//        if (esPrimo(numero)) {
+//            println(numero)
+//        }
+//    }
+//}
+//
+//fun main() {
+//    mostrarPrimosHasta(100)
+//}
+//EJERCICIO 4
+
+//EJERCICIO 5
+//fun main(){
+//    val horaDiurna: Double = 10.0
+//    val horaNocturna: Double = 13.5
+//    val festDiurna: Double = 0.10
+//    val festNocturna: Double = 0.15
+//    println("Dime las horas que vas a trabajar: ")
+//    var horas: Double = readln().toDouble()
+//    var jornal: Double = 0.0}
+//EJERCICIO 6
+//fun main() {
+//
+//}
